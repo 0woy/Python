c348b08a25e686afa8411f8d5a1f4227b9257d95
0woy <qkrdbsdk1030@naver.com>
2023-03-23 15:30:08 +0900

    산술연산

---
+++ Arithmetic.py
@@ -0,0 +1,11 @@
+n1 = 100
+n2 =200
+r1 = n1+n2
+r2 = n1 - n2
+r3 = n1 * n2
+r4 = n1 / n2
+print(str(n1)+" + "+str(n2)+" = "+str(r1))
+print(str(n1)+" - "+str(n2)+" = "+str(r2))
+print(str(n1)+" * "+str(n2)+" = "+str(r3))
+print(str(n1)+" / "+str(n2)+" = "+str(r4))
+print("순천향대학교 컴퓨터소프트웨어공학과 20204624 박윤아")
